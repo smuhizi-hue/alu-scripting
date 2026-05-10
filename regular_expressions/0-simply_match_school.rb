7221d837994425ac9e1e63e4e8b0558e6477e81d
shaun <s.muhizi@alustudent.com>
2026-05-11 00:06:28 +0300

    Update School matching script

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
 # This script accepts one argument and passes it to a regex matching method
-# The regex /School/ matches the literal word "School"
 puts ARGV[0].scan(/School/).join
