23fa27ea034b287a4cf584e8ded705769dd052be
shaun <s.muhizi@alustudent.com>
2026-05-10 23:39:35 +0300

    Update repetition token regex

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#This regex matches 'hb'followed by 't'reapting 2 to 5 times, then 'n'.
+# This regex matches 'hb' followed by 't' repeating 2 to 5 times, then 'n'.
 puts ARGV[0].scan(/hbt{2,5}n/).join
