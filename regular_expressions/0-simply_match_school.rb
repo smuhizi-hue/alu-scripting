b243ffbe649d8cabec7940599eff4bf48b6877c8
smuhizi-hue <s.muhizi@alustudent.com>
2026-05-11 00:00:16 +0300

    Delete regular_expressions/0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regex matching method
-# The regex /School/ matches the literal word "School"
-puts ARGV[0].scan(/School/).join
